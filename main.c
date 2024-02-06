ec4d5225429b9408d6f4de3c5a166c95c55281cb
oMAcS <david232818@gmail.com>
2024-02-06 17:16:57 +0900

    [MODIFY/main.c] Remove irrelevant header inclusion

--- main.c
+++ main.c
@@ -2,13 +2,11 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
-#include <netdb.h>
-#include <ifaddrs.h>
-#include <unistd.h>
 #include <linux/if_link.h>
 #include <linux/wireless.h>
 
