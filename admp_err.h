f692e1148d4559ee38adfc68446c979eb2bdf68a
oMAcS <david232818@gmail.com>
2024-02-09 01:25:59 +0900

    [MODIFY/admp_err.h] change ADMP_ERR_INVALID_FRAME to ADMP_ERR_UNKOWN_FRAME

--- admp_err.h
+++ admp_err.h
@@ -4,17 +4,17 @@
 /* error types for admp */
 
 typedef enum {
-	ADMP_SUCCESS = 0,
-	ADMP_FAIL = 1,
-	ADMP_ERR_INVALID_ARGS = 2,
-	ADMP_ERR_SOCKET,
-	ADMP_ERR_IWCMD,
-	ADMP_ERR_IFCMD,
-	ADMP_ERR_CLOSE,
-	ADMP_ERR_INVALID_FRAME,
-	ADMP_ERR_COUNT
+    ADMP_SUCCESS = 0,
+    ADMP_FAIL = 1,
+    ADMP_ERR_INVALID_ARGS = 2,
+    ADMP_ERR_SOCKET,
+    ADMP_ERR_IWCMD,
+    ADMP_ERR_IFCMD,
+    ADMP_ERR_CLOSE,
+    ADMP_ERR_UNKNOWN_FRAME,
+    ADMP_ERR_COUNT
 } admp_res_t;
 
-void admp_printerr(admp_res_t, const char *);
+void admp_printerr(const admp_res_t, const char *);
 
 #endif
