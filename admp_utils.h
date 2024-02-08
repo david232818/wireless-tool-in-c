57b3bed9426064d00aa901748011f2addb980d32
oMAcS <david232818@gmail.com>
2024-02-09 01:31:54 +0900

    [MODIFY/admp_utils.h] now ADMP_IS_X_IN_RANGE macro condition is min <= x < max

--- admp_utils.h
+++ admp_utils.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define ADMP_IS_X_IN_RANGE(min, x, max) (((x) > (min)) && ((x) < (max)))
+#define ADMP_IS_X_IN_RANGE(min, x, max) (((x) >= (min)) && ((x) < (max)))
 
 void admp_hexdump(const void *, size_t);
 
