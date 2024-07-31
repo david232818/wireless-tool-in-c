85491d1d1a5fb1ae9601f78afd23128fc1463e82
david232818 <david232818@gmail.com>
2024-08-01 07:33:20 +0900

    Add create, delete, and search api for AP list

--- include/wlt_ieee80211_ap.h
+++ include/wlt_ieee80211_ap.h
@@ -11,10 +11,21 @@ struct wlt_ieee80211_ap {
     uint8_t bssid[7];		/* 6th byte to check bssid is not NULL */
     char ssid[WLT_IEEE80211_APNAMSIZ];
 
-    struct ll_node link;
+    struct ll_node node;
 };
 
+enum ap_search_flag {
+    WLT_IEEE80211_AP_NOT_FOUND = 0,
+    WLT_IEEE80211_AP_FOUND = 1,
+    WLT_SEARCH_FLAG_MAX
+};
+
+int wlt_ieee80211_ap_search(struct wlt_ieee80211_ap *aplist,
+			    struct wlt_ieee80211_ap *target);
+void wlt_ieee80211_ap_add_tail(struct wlt_ieee80211_ap *aplist,
+			       struct wlt_ieee80211_ap *ap);
 struct wlt_ieee80211_ap *wlt_ieee80211_ap_init(void);
-void wlt_ieee80211_ap_destroy(struct wlt_ieee80211_ap *ap);
+void wlt_ieee80211_ap_destroy(void *ap);
+void wlt_ieee80211_ap_list_clear(struct wlt_ieee80211_ap *aplist);
 
 #endif
