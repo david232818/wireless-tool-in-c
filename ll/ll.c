d7700bce8bd41845e5b1b80d5bc3e220ae9d8530
david232818 <david232818@gmail.com>
2024-09-11 03:38:38 +0900

    Add inlines and node insertions

--- ll/ll.c
+++ ll/ll.c
@@ -4,45 +4,25 @@
 #include <stddef.h>
 #include "ll.h"
 
-void ll_node_remove(struct ll_node *node)
-{
-    node->next->prev = node->prev;
-    node->prev->next = node->next;
-    node->head = node;
-    node->prev = node;
-    node->next = node;
-}
+extern inline void ll_node_remove(struct ll_node *node);
 
 void ll_node_clear(struct ll_node *list, void (*destroy)(void *obj))
 {
     struct ll_node *curr, *next;
 
-    curr = list->head->next;
-    do {
-	next = curr->next;
+    LL_NODE_FOREACH(list, curr, next) {
 	ll_node_remove(curr);
 	destroy(curr->obj);
-	curr = next;
-    } while (next != list->head);
+    }
 }
+
+extern inline void ll_node_insert(struct ll_node *prev,
+				  struct ll_node *node,
+				  struct ll_node *next);
 
 void ll_node_add_tail(struct ll_node *list, struct ll_node *node)
 {
-    node->prev = list->prev;
-    node->next = list->head;
-    list->prev->next = node;
-    list->prev = node;
-    node->head = list->head;
+    ll_node_insert(list->prev, node, list);
 }
 
-int ll_node_init(struct ll_node *node, size_t objoff)
-{
-    if (node == NULL)
-	return -1;
-
-    node->head = node;
-    node->prev = node;
-    node->next = node;
-    node->obj = (char *) node - objoff;
-    return 0;
-}
+extern inline void ll_node_init(struct ll_node *node, void *obj);
